6fddfd8bbc1e1e15a9ab487806a14421b4013feb
Dino <crescimbenidino@gmail.com>
2022-02-23 17:56:33 -0300

    Implemented tests for user input validation

---
+++ test/models.js
@@ -0,0 +1,66 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const db = require('../api/models/_db');
+const { User } = require('../api/models/');
+const { Movie } = require('../api/models/');
+
+describe('User model', () => {
+  beforeEach(() => {
+    return db.sync({ force: true });
+  });
+
+  afterEach(() => {
+    return db.sync({ force: true });
+  });
+
+  it('Should create a user', () => {
+    return User.create({
+      username: 'Dino',
+      password: 'test',
+      email: 'dino@example.com',
+    })
+      .then((createdUser) => createdUser.dataValues)
+      .then((user) => {
+        expect(user).to.have.property('id');
+        expect(user).to.have.property('username', 'Dino');
+        expect(user).to.have.property('password', 'test');
+      })
+      .catch((err) => console.log(err));
+  });
+
+  it(`username can't be null`, () => {
+    return User.create({
+      password: 'test',
+      email: 'dino@example.com',
+    })
+      .then((response) => console.log(response))
+      .catch((err) => {
+        expect(err.errors[0].type).to.equal('notNull Violation');
+      });
+  });
+
+  it(`password can't be null`, () => {
+    return User.create({
+      username: 'Dino',
+      email: 'dino@example.com',
+    })
+      .then((response) => console.log(response))
+      .catch((err) => {
+        expect(err.errors[0].type).to.equal('notNull Violation');
+      });
+  });
+
+  it(`email can't be null`, () => {
+    return User.create({
+      user: 'dino',
+      password: 'test',
+    })
+      .then((response) => console.log(response))
+      .catch((err) => {
+        expect(err.errors[0].type).to.equal('notNull Violation');
+      });
+  });
+});

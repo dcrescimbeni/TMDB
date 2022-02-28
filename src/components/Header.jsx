3754d604374389a6c0af89b7bfce2fc24e78b822
Dino <crescimbenidino@gmail.com>
2022-02-28 11:09:05 -0300

    Implemented basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import styled from 'styled-components/macro';
 
 const Header = ({ user }) => {
-  return <header>Header</header>;
+  return (
+    <HeaderWrapper>
+      <div>MUUBI</div>
+      <div>Sign up / Log in</div>
+    </HeaderWrapper>
+  );
 };
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 12px 24px;
+`;
 
 export default Header;

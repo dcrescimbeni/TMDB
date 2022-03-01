cb8c90a768524831932dc1b40a361c24e0207241
Dino <crescimbenidino@gmail.com>
2022-03-01 09:37:23 -0300

    Can create new users

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,23 +1,69 @@
+import styled from 'styled-components/macro';
+import axios from 'axios';
+
+import useInput from '../hooks/useInput';
+import MainButton from '../commons/MainButton';
+import SecondaryButton from '../commons/SecondaryButton';
+
 const SignUp = () => {
+  const username = useInput('');
+  const password = useInput('');
+  const passwordAgain = useInput('');
+  const email = useInput('');
+  const emailAgain = useInput('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post('/api/users/new', {
+        username: username.value,
+        password: password.value,
+        email: email.value,
+      })
+      .then((res) => res.data)
+      .then((newUser) => {
+        console.log(newUser);
+      });
+  };
+
   return (
-    <div>
-      <div>
-        <h3>Sign up</h3>
-        <form>
+    <MainWrapper>
+      <h3>Sign up</h3>
+      <form onSubmit={handleSubmit}>
+        <FormFieldsWrapper>
           <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Username" {...username} />
           <br />
           <label htmlFor="password">Password</label>
-          <input type="text" placeholder="Password" />
-          <input type="text" placeholder="Enter again" />
+          <input type="text" placeholder="Password" {...password} />
+          <input type="text" placeholder="Enter again" {...passwordAgain} />
           <br />
           <label htmlFor="email">Email</label>
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Enter again" />
-        </form>
-      </div>
-    </div>
+          <input type="text" placeholder="Email" {...email} />
+          <input type="text" placeholder="Enter again" {...emailAgain} />
+          <div>
+            <MainButton onClick={handleSubmit}>Sign up</MainButton>
+            <SecondaryButton>Log in</SecondaryButton>
+          </div>
+        </FormFieldsWrapper>
+      </form>
+    </MainWrapper>
   );
 };
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  border-radius: 10px;
+  background-color: #f6f6f6;
+  padding: 50px;
+`;
+
+const FormFieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default SignUp;

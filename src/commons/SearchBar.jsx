1306cbcd8c700537b58edb0a30515917d09997ca
Dino <crescimbenidino@gmail.com>
2022-03-02 11:46:54 -0300

    Added styles to search bar

--- src/commons/SearchBar.jsx
+++ src/commons/SearchBar.jsx
@@ -1,4 +1,6 @@
 import useInput from '../hooks/useInput';
+import styled from 'styled-components/macro';
+import { BiSearchAlt } from 'react-icons/bi';
 
 const SearchBar = ({ handleSubmit }) => {
   const searchQuery = useInput('');
@@ -10,15 +12,37 @@ const SearchBar = ({ handleSubmit }) => {
           handleSubmit(event, searchQuery.value);
         }}
       >
-        <input
-          type="text"
-          name="search"
-          {...searchQuery}
-          placeholder="Search..."
-        />
+        <SearchBarWrapper>
+          <SearchInput
+            type="text"
+            name="search"
+            {...searchQuery}
+            placeholder="Search..."
+          />
+          <BiSearchAlt color="#9c1de7" size="2rem" />
+        </SearchBarWrapper>
       </form>
     </div>
   );
 };
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+`;
+
+const SearchInput = styled.input`
+  width: 580px;
+  height: 50px;
+  border-radius: 30px;
+  padding-left: 55px;
+  border: 2px solid #9c1de7;
+  margin-left: -50px;
+
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default SearchBar;
